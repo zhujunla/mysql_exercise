e9389f80c38d84921e0551df004ea64159f136fd
hero <>
2018-06-06 11:54:12 +0800

    log日志

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+// var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
-
+var log4js = require('./log4');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
 
-// view engine setup
+const logger = log4js.getLogger()//根据需要获取logger
+
+const errlogger = log4js.getLogger('err')
+const othlogger = log4js.getLogger('oth')
+
+log4js.useLogger(app,logger)//这样会自动记录每次请求信息，放在其他use上面
+
+console.info(logger)
+logger.info('test info 1')
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
